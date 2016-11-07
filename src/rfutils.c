5d4bb0881d6fc7a9c1a5cfb0dd35549364f1ebfb
James Browne <jbrowne6@jhu.edu>
2016-11-07 10:52:32 -0500

    new tests

--- src/rfutils.c
+++ src/rfutils.c
@@ -14,329 +14,342 @@
 #include <R.h>
 #include "rf.h"
 
-
 void zeroInt(int *x, int length) {
-		memset(x, 0, length * sizeof(int));
+	memset(x, 0, length * sizeof(int));
 }
 
 void zeroDouble(double *x, int length) {
-		memset(x, 0, length * sizeof(double));
+	memset(x, 0, length * sizeof(double));
 }
 
 void createClass(double *x, int realN, int totalN, int mdim) {
-		/* Create the second class by bootstrapping each variable independently. */
-		int i, j, k;
-		for (i = realN; i < totalN; ++i) {
-				for (j = 0; j < mdim; ++j) {
-						k = (int) (unif_rand() * realN);
-						x[j + i * mdim] = x[j + k * mdim];
-				}
+	/* Create the second class by bootstrapping each variable independently. */
+	int i, j, k;
+	for (i = realN; i < totalN; ++i) {
+		for (j = 0; j < mdim; ++j) {
+			k = (int) (unif_rand() * realN);
+			x[j + i * mdim] = x[j + k * mdim];
 		}
+	}
 }
 
 void normClassWt(int *cl, const int nsample, const int nclass,
-				const int useWt, double *classwt, int *classFreq) {
-		int i;
-		double sumwt = 0.0;
+		const int useWt, double *classwt, int *classFreq) {
+	int i;
+	double sumwt = 0.0;
 
-		if (useWt) {
-				/* Normalize user-supplied weights so they sum to one. */
-				for (i = 0; i < nclass; ++i) sumwt += classwt[i];
-				for (i = 0; i < nclass; ++i) classwt[i] /= sumwt;
-		} else {
-				for (i = 0; i < nclass; ++i) {
-						classwt[i] = ((double) classFreq[i]) / nsample;
-				}
-		}
+	if (useWt) {
+		/* Normalize user-supplied weights so they sum to one. */
+		for (i = 0; i < nclass; ++i) sumwt += classwt[i];
+		for (i = 0; i < nclass; ++i) classwt[i] /= sumwt;
+	} else {
 		for (i = 0; i < nclass; ++i) {
-				classwt[i] = classFreq[i] ? classwt[i] * nsample / classFreq[i] : 0.0;
+			classwt[i] = ((double) classFreq[i]) / nsample;
 		}
+	}
+	for (i = 0; i < nclass; ++i) {
+		classwt[i] = classFreq[i] ? classwt[i] * nsample / classFreq[i] : 0.0;
+	}
 }
 
 void randx(double *x, double *XA, const int mdim, const int nsample, const int mtry, const int rerf){
-		int m,n,p,q;
-		double dotProd;
-		double rerf_p = (double) rerf / 100;
-
-		//	double *A = (double *) calloc (mdim * mtry, sizeof (double));
-		double A[mdim*mtry];	
-		zeroDouble(XA, mtry*nsample);
-		//	Rprintf("\n start makeA");
-		int target_used = (mdim-1)*mtry;
-		int num_used = 0;
-		if (rerf == 20){
-				zeroDouble(A, mdim*mtry);
-				for (int q = 0; q<mtry; q++){
-						A[q*(mtry+1)]=1;
-				}
-		} else{
+	int m,n,p,q;
+	double dotProd;
+	double rerf_p = (double) rerf / 100;
+	int err_print = 0; //if 1 print. else don't print.
+	double A[mdim*mtry];
+	zeroDouble(XA, mtry*nsample);
+	int target_used = mdim*(mtry-1);
+//				int target_used = mdim;
+	float percent_target = (float)target_used/(mdim*mtry);
+	int num_used = 0;
+	if (rerf == 20){
+		zeroDouble(A, mdim*mtry);
+		for (int q = 0; q<mtry; q++){
+			A[q*(mtry+1)]=1;
+		}
+	} else if (rerf == 3 || rerf == 4 || rerf == 5){
+target_used = mdim*(mtry-1);
+percent_target = (float)target_used/(mdim*mtry);
 		while(num_used != target_used){
-num_used = 0;
-
-				zeroDouble(A, mdim*mtry);
-				for (int q = 0; q < mdim*mtry; q++)
-				{
-						if(unif_rand() > 1/((double)mdim)){
-								num_used++;
-								if(unif_rand()<.5){
-										A[q]=-1;
-								}else{
-										A[q]=1;
-								}
-						}
+			num_used = 0;
+			zeroDouble(A, mdim*mtry);
+			for (int q = 0; q < mdim*mtry; q++)
+			{
+				if(unif_rand() < percent_target){
+					num_used++;
+					if(unif_rand()<.5){
+						A[q]=-1;
+					}else{
+						A[q]=1;
+					}
 				}
-		}}
-
-Rprintf("\n\n\n");
-		for (int q = 0; q < mdim; q++)
-		{
-				Rprintf("\n");
-for (int t = 0; t < mtry; t++){
-		Rprintf("%f ", A[q*mtry + t]);
-}
-
+			}
 		}
-Rprintf("\n\n");
+	}else if (rerf == 6 || rerf == 7 || rerf == 8){
+target_used = mdim;
+percent_target = (float)target_used/(mdim*mtry);
+		while(num_used != target_used){
+			num_used = 0;
+			zeroDouble(A, mdim*mtry);
+			for (int q = 0; q < mdim*mtry; q++)
+			{
+				if(unif_rand() < percent_target){
+					num_used++;
+					if(unif_rand()<.5){
+						A[q]=-1;
+					}else{
+						A[q]=1;
+					}
+				}
+			}
+		}
+	}
+
+	// Do this for all runs.
+	for (n = 0; n < nsample; n++){
+		for (p = 0; p < mtry; p++){
+			for (m=0; m<mdim; m++){
+				XA[n*mtry+p]+= x[n*mdim+m]*A[p*mdim+m];
+			}
+		}
+	}
+	if (err_print == 1){
+		Rprintf("\n\n\n");
+		for (int q = 0; q < mtry; q++)
+		{
+			Rprintf("\n");
+			for (int t = 0; t < mdim; t++){
+				Rprintf("%f ", A[q*mtry + t]);
+			}
+		}
+		Rprintf("\n\n");
 		for (int q = 0; q < nsample; q++)
 		{
-				Rprintf("\n");
-for (int t = 0; t < mdim; t++){
-		Rprintf("%f ", x[q*mdim + t]);
-}
-
+			Rprintf("\n");
+			for (int t = 0; t < mdim; t++){
+				Rprintf("%f ", x[q*mdim + t]);
+			}
 		}
-
-
-		for (n = 0; n < nsample; n++){
-				for (p = 0; p < mtry; p++){
-						for (m=0; m<mdim; m++){
-								XA[n*mtry+p]+= x[n*mdim+m]*A[p*mdim+m];
-						
-						}
-						
-				}
-		}
-//free(A);
-Rprintf("\n\n");
+		Rprintf("\n\n");
 		for (int q = 0; q < nsample; q++)
 		{
-				Rprintf("\n");
-for (int t = 0; t < mtry; t++){
-		Rprintf("%f ", XA[q*mtry + t]);
-}
-
+			Rprintf("\n");
+			for (int t = 0; t < mtry; t++){
+				Rprintf("%f ", XA[q*mtry + t]);
+			}
 		}
+	}
 }
 
 void makeA(double *x, const int mdim, const int nsample, int *cat, int *a,
-				int *b) {
-		/* makeA() constructs the mdim by nsample integer array a.  For each
-		   numerical variable with values x(m, n), n=1, ...,nsample, the x-values
-		   are sorted from lowest to highest.  Denote these by xs(m, n).  Then
-		   a(m,n) is the case number in which xs(m, n) occurs. The b matrix is
-		   also contructed here.  If the mth variable is categorical, then
-		   a(m, n) is the category of the nth case number. */
-		int i, j, n1, n2, *index;
-		double *v;
+		int *b) {
+	/* makeA() constructs the mdim by nsample integer array a.  For each
+	   numerical variable with values x(m, n), n=1, ...,nsample, the x-values
+	   are sorted from lowest to highest.  Denote these by xs(m, n).  Then
+	   a(m,n) is the case number in which xs(m, n) occurs. The b matrix is
+	   also contructed here.  If the mth variable is categorical, then
+	   a(m, n) is the category of the nth case number. */
+	int i, j, n1, n2, *index;
+	double *v;
 
-		v     = (double *) Calloc(nsample, double);
-		index = (int *) Calloc(nsample, int);
+	v     = (double *) Calloc(nsample, double);
+	index = (int *) Calloc(nsample, int);
 
-		for (i = 0; i < mdim; ++i) {
-				if (cat[i] == 1) { /* numerical predictor */
-						for (j = 0; j < nsample; ++j) {
-								v[j] = x[i + j * mdim];
-								index[j] = j + 1;
-						}
-						R_qsort_I(v, index, 1, nsample);
+	for (i = 0; i < mdim; ++i) {
+		if (cat[i] == 1) { /* numerical predictor */
+			for (j = 0; j < nsample; ++j) {
+				v[j] = x[i + j * mdim];
+				index[j] = j + 1;
+			}
+			R_qsort_I(v, index, 1, nsample);
 
-						/*  this sorts the v(n) in ascending order. index(n) is the case
-							number of that v(n) nth from the lowest (assume the original
-							case numbers are 1,2,...).  */
-						for (j = 0; j < nsample-1; ++j) {
-								n1 = index[j];
-								n2 = index[j + 1];
-								a[i + j * mdim] = n1;
-								if (j == 0) b[i + (n1-1) * mdim] = 1;
-								b[i + (n2-1) * mdim] =  (v[j] < v[j + 1]) ?
-										b[i + (n1-1) * mdim] + 1 : b[i + (n1-1) * mdim];
-						}
-						a[i + (nsample-1) * mdim] = index[nsample-1];
-				} else { /* categorical predictor */
-						for (j = 0; j < nsample; ++j)
-								a[i + j*mdim] = (int) x[i + j * mdim];
-				}
+			/*  this sorts the v(n) in ascending order. index(n) is the case
+				number of that v(n) nth from the lowest (assume the original
+				case numbers are 1,2,...).  */
+			for (j = 0; j < nsample-1; ++j) {
+				n1 = index[j];
+				n2 = index[j + 1];
+				a[i + j * mdim] = n1;
+				if (j == 0) b[i + (n1-1) * mdim] = 1;
+				b[i + (n2-1) * mdim] =  (v[j] < v[j + 1]) ?
+					b[i + (n1-1) * mdim] + 1 : b[i + (n1-1) * mdim];
+			}
+			a[i + (nsample-1) * mdim] = index[nsample-1];
+		} else { /* categorical predictor */
+			for (j = 0; j < nsample; ++j)
+				a[i + j*mdim] = (int) x[i + j * mdim];
 		}
-		Free(index);
-		Free(v);
+	}
+	Free(index);
+	Free(v);
 }
 
-
 void modA(int *a, int *nuse, const int nsample, const int mdim,
-				int *cat, const int maxcat, int *ncase, int *jin) {
-		int i, j, k, m, nt;
+		int *cat, const int maxcat, int *ncase, int *jin) {
+	int i, j, k, m, nt;
 
-		*nuse = 0;
-		for (i = 0; i < nsample; ++i) if (jin[i]) (*nuse)++;
+	*nuse = 0;
+	for (i = 0; i < nsample; ++i) if (jin[i]) (*nuse)++;
 
-		for (i = 0; i < mdim; ++i) {
-				k = 0;
-				nt = 0;
-				if (cat[i] == 1) {
-						for (j = 0; j < nsample; ++j) {
-								if (jin[a[i + k * mdim] - 1]) {
-										a[i + nt * mdim] = a[i + k * mdim];
-										k++;
-								} else {
-										for (m = 0; m < nsample - k; ++m) {
-												if (jin[a[i + (k + m) * mdim] - 1]) {
-														a[i + nt * mdim] = a[i + (k + m) * mdim];
-														k += m + 1;
-														break;
-												}
-										}
-								}
-								nt++;
-								if (nt >= *nuse) break;
+	for (i = 0; i < mdim; ++i) {
+		k = 0;
+		nt = 0;
+		if (cat[i] == 1) {
+			for (j = 0; j < nsample; ++j) {
+				if (jin[a[i + k * mdim] - 1]) {
+					a[i + nt * mdim] = a[i + k * mdim];
+					k++;
+				} else {
+					for (m = 0; m < nsample - k; ++m) {
+						if (jin[a[i + (k + m) * mdim] - 1]) {
+							a[i + nt * mdim] = a[i + (k + m) * mdim];
+							k += m + 1;
+							break;
 						}
+					}
 				}
+				nt++;
+				if (nt >= *nuse) break;
+			}
 		}
-		if (maxcat > 1) {
-				k = 0;
-				nt = 0;
-				for (i = 0; i < nsample; ++i) {
-						if (jin[k]) {
-								k++;
-								ncase[nt] = k;
-						} else {
-								for (j = 0; j < nsample - k; ++j) {
-										if (jin[k + j]) {
-												ncase[nt] = k + j + 1;
-												k += j + 1;
-												break;
-										}
-								}
-						}
-						nt++;
-						if (nt >= *nuse) break;
+	}
+	if (maxcat > 1) {
+		k = 0;
+		nt = 0;
+		for (i = 0; i < nsample; ++i) {
+			if (jin[k]) {
+				k++;
+				ncase[nt] = k;
+			} else {
+				for (j = 0; j < nsample - k; ++j) {
+					if (jin[k + j]) {
+						ncase[nt] = k + j + 1;
+						k += j + 1;
+						break;
+					}
 				}
+			}
+			nt++;
+			if (nt >= *nuse) break;
 		}
+	}
 }
 
 void Xtranslate(double *x, int mdim, int nrnodes, int nsample,
-				int *bestvar, int *bestsplit, int *bestsplitnext,
-				double *xbestsplit, int *nodestatus, int *cat, int treeSize) {
-		/*
-		   this subroutine takes the splits on numerical variables and translates them
-		   back into x-values.  It also unpacks each categorical split into a
-		   32-dimensional vector with components of zero or one--a one indicates that
-		   the corresponding category goes left in the split.
-		 */
+		int *bestvar, int *bestsplit, int *bestsplitnext,
+		double *xbestsplit, int *nodestatus, int *cat, int treeSize) {
+	/*
+	   this subroutine takes the splits on numerical variables and translates them
+	   back into x-values.  It also unpacks each categorical split into a
+	   32-dimensional vector with components of zero or one--a one indicates that
+	   the corresponding category goes left in the split.
+	   */
 
-		int i, m;
+	int i, m;
 
-		for (i = 0; i < treeSize; ++i) {
-				if (nodestatus[i] == 1) {
-						m = bestvar[i] - 1;
-						if (cat[m] == 1) {
-								xbestsplit[i] = 0.5 * (x[m + (bestsplit[i] - 1) * mdim] +
-												x[m + (bestsplitnext[i] - 1) * mdim]);
-						} else {
-								xbestsplit[i] = (double) bestsplit[i];
-						}
-				}
+	for (i = 0; i < treeSize; ++i) {
+		if (nodestatus[i] == 1) {
+			m = bestvar[i] - 1;
+			if (cat[m] == 1) {
+				xbestsplit[i] = 0.5 * (x[m + (bestsplit[i] - 1) * mdim] +
+						x[m + (bestsplitnext[i] - 1) * mdim]);
+			} else {
+				xbestsplit[i] = (double) bestsplit[i];
+			}
 		}
+	}
 }
 
 void permuteOOB(int m, double *x, int *in, int nsample, int mdim) {
-		/* Permute the OOB part of a variable in x.
-		 * Argument:
-		 *   m: the variable to be permuted
-		 *   x: the data matrix (variables in rows)
-		 *   in: vector indicating which case is OOB
-		 *   nsample: number of cases in the data
-		 *   mdim: number of variables in the data
-		 */
-		double *tp, tmp;
-		int i, last, k, nOOB = 0;
+	/* Permute the OOB part of a variable in x.
+	 * Argument:
+	 *   m: the variable to be permuted
+	 *   x: the data matrix (variables in rows)
+	 *   in: vector indicating which case is OOB
+	 *   nsample: number of cases in the data
+	 *   mdim: number of variables in the data
+	 */
+	double *tp, tmp;
+	int i, last, k, nOOB = 0;
 
-		tp = (double *) Calloc(nsample, double);
+	tp = (double *) Calloc(nsample, double);
 
-		for (i = 0; i < nsample; ++i) {
-				/* make a copy of the OOB part of the data into tp (for permuting) */
-				if (in[i] == 0) {
-						tp[nOOB] = x[m + i*mdim];
-						nOOB++;
-				}
+	for (i = 0; i < nsample; ++i) {
+		/* make a copy of the OOB part of the data into tp (for permuting) */
+		if (in[i] == 0) {
+			tp[nOOB] = x[m + i*mdim];
+			nOOB++;
 		}
-		/* Permute tp */
-		last = nOOB;
-		for (i = 0; i < nOOB; ++i) {
-				k = (int) last * unif_rand();
-				tmp = tp[last - 1];
-				tp[last - 1] = tp[k];
-				tp[k] = tmp;
-				last--;
-		}
+	}
+	/* Permute tp */
+	last = nOOB;
+	for (i = 0; i < nOOB; ++i) {
+		k = (int) last * unif_rand();
+		tmp = tp[last - 1];
+		tp[last - 1] = tp[k];
+		tp[k] = tmp;
+		last--;
+	}
 
-		/* Copy the permuted OOB data back into x. */
-		nOOB = 0;
-		for (i = 0; i < nsample; ++i) {
-				if (in[i] == 0) {
-						x[m + i*mdim] = tp[nOOB];
-						nOOB++;
-				}
+	/* Copy the permuted OOB data back into x. */
+	nOOB = 0;
+	for (i = 0; i < nsample; ++i) {
+		if (in[i] == 0) {
+			x[m + i*mdim] = tp[nOOB];
+			nOOB++;
 		}
-		Free(tp);
+	}
+	Free(tp);
 }
 
 /* Compute proximity. */
 void computeProximity(double *prox, int oobprox, int *node, int *inbag,
-				int *oobpair, int n) {
-		/* Accumulate the number of times a pair of points fall in the same node.
+		int *oobpair, int n) {
+	/* Accumulate the number of times a pair of points fall in the same node.
 prox:    n x n proximity matrix
 oobprox: should the accumulation only count OOB cases? (0=no, 1=yes)
 node:    vector of terminal node labels
 inbag:   indicator of whether a case is in-bag
 oobpair: matrix to accumulate the number of times a pair is OOB together
 n:       total number of cases
-		 */
-		int i, j;
-		for (i = 0; i < n; ++i) {
-				for (j = i+1; j < n; ++j) {
-						if (oobprox) {
-								if (! (inbag[i] > 0 || inbag[j] > 0) ) {
-										oobpair[j*n + i] ++;
-										oobpair[i*n + j] ++;
-										if (node[i] == node[j]) {
-												prox[j*n + i] += 1.0;
-												prox[i*n + j] += 1.0;
-										}
-								}
-						} else {
-								if (node[i] == node[j]) {
-										prox[j*n + i] += 1.0;
-										prox[i*n + j] += 1.0;
-								}
-						}
+*/
+	int i, j;
+	for (i = 0; i < n; ++i) {
+		for (j = i+1; j < n; ++j) {
+			if (oobprox) {
+				if (! (inbag[i] > 0 || inbag[j] > 0) ) {
+					oobpair[j*n + i] ++;
+					oobpair[i*n + j] ++;
+					if (node[i] == node[j]) {
+						prox[j*n + i] += 1.0;
+						prox[i*n + j] += 1.0;
+					}
 				}
+			} else {
+				if (node[i] == node[j]) {
+					prox[j*n + i] += 1.0;
+					prox[i*n + j] += 1.0;
+				}
+			}
 		}
+	}
 }
 
 double pack(const int nBits, const int *bits) {
-		int i = nBits - 1;
-		double pack = bits[i];
-		for (i = nBits - 1; i > 0; --i) pack = 2.0 * pack + bits[i - 1];
-		return(pack);
+	int i = nBits - 1;
+	double pack = bits[i];
+	for (i = nBits - 1; i > 0; --i) pack = 2.0 * pack + bits[i - 1];
+	return(pack);
 }
 
 void unpack(const double pack, const int nBits, int *bits) {
-		int i;
-		double x = pack;
-		for (i = 0; i <= nBits; ++i) {
-				bits[i] = ((unsigned long) x & 1) ? 1 : 0;
-				x = x / 2;
-		}
+	int i;
+	double x = pack;
+	for (i = 0; i <= nBits; ++i) {
+		bits[i] = ((unsigned long) x & 1) ? 1 : 0;
+		x = x / 2;
+	}
 }
 
 /*
@@ -351,9 +364,8 @@ void unpack(const double pack, const int nBits, int *bits) {
    int i;
    for (i = 0; i < nBits; pack >>= 1, ++i) bits[i] = pack & 1;
    }
- */
+   */
 
 void F77_NAME(unpack)(double *pack, int *nBits, int *bits) {
-		unpack(*pack, *nBits, bits);
+	unpack(*pack, *nBits, bits);
 }
-
